2056237ad4d9bd476802f8fdc413d0fd717b6722
George Ornbo <george@shapeshed.com>
2014-02-04 20:16:35 +0000

    [refactor] src directory

---
+++ src/main.js
@@ -0,0 +1,12 @@
+var $ = require('jquery')(window);
+var Backbone = require('backbone');
+var Wreqr = require('../node_modules/backbone.marionette/node_modules/backbone.wreqr');
+var Marionette = require('backbone.marionette');
+Marionette.$ = Backbone.$ = $;
+Backbone.Wreqr = Wreqr;
+
+var MyApp = new Marionette.Application();
+MyApp.addInitializer(function(options){
+  console.log('initialised app');
+});
+MyApp.start();
