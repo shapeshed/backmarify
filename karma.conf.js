f99b3ccbcc2278d0914a46d673b1440d3baa1e87
George Ornbo <george@shapeshed.com>
2014-02-04 20:17:14 +0000

    [refactor] set logging to info

--- karma.conf.js
+++ karma.conf.js
@@ -11,7 +11,7 @@ module.exports = function(config) {
     reporters: ['progress'],
     port: 9876,
     colors: true,
-    logLevel: config.LOG_INFO,
+    logLevel: config.LOG_DEBUG,
     autoWatch: true,
     browsers: ['PhantomJS'],
     captureTimeout: 60000,
